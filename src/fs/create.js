1373b940e76bd0f4bc1a99058f03d76edcb225f0
Vladyslav <dallasx049@gmail.com>
2025-10-22 18:13:14 +0300

    feat: implement fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { appendFile } from 'fs/promises';
+import { join } from 'path';
+
 const create = async () => {
-  // Write your code here
+  const filePath = join(import.meta.dirname, 'files', 'fresh.txt');
+
+  try {
+    await appendFile(filePath, 'I am fresh and young', { flag: 'ax' });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
