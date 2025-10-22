c4e1d50d00062b2a5bbfc291c0b79afbbeaa16de
Vladyslav <dallasx049@gmail.com>
2025-10-22 18:24:37 +0300

    feat: implement fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { join } from 'path';
+import { cp } from 'fs/promises';
+
 const copy = async () => {
-  // Write your code here
+  const srcFolderPath = join(import.meta.dirname, 'files');
+  const destFolderPath = join(import.meta.dirname, 'files_copy');
+
+  try {
+    await cp(srcFolderPath, destFolderPath, {
+      force: false,
+      errorOnExist: true,
+      recursive: true,
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
