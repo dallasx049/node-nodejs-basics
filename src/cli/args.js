ca91b6d8cef15bf8d3909509b38830bde40ac0cb
Vladyslav <dallasx049@gmail.com>
2025-10-22 19:53:49 +0300

    feat: implement cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv
+    .slice(2)
+    .map((arg) => arg.startsWith('--') ? arg.slice(2) : arg);
+  const temp = [];
+
+  while (args.length) {
+    temp.push(args.splice(0, 2).join(' is '));
+  }
+
+  console.log(temp.join(', '));
 };
 
 parseArgs();
