977e3b558642e984300f74e5d333c87448b7b4fc
Vladyslav <dallasx049@gmail.com>
2025-10-23 13:59:31 +0300

    feat: implement stream write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+
 const write = async () => {
-  // Write your code here
+  const filePath = join(import.meta.dirname, 'files', 'fileToWrite.txt');
+  const ws = createWriteStream(filePath);
+
+  process.stdin.pipe(ws);
 };
 
 await write();
