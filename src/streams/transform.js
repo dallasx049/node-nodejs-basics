09626c37b6d0120588d8977a8f21319c832c1c9d
Vladyslav <dallasx049@gmail.com>
2025-10-23 14:25:26 +0300

    feat: implement stream transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+class Reverse extends Transform {
+  constructor() {
+    super();
+  }
+
+  _transform(data, encoding, callback) {
+    this.push(data.toString().split('').reverse().join(''));
+    callback();
+  }
+}
+
 const transform = async () => {
-  // Write your code here
+  try {
+    await pipeline(
+      process.stdin,
+      new Reverse(),
+      process.stdout,
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await transform();
