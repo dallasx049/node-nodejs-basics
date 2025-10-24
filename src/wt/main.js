1d7489e480306f9cb9cbd3ff49a89bf7122db2a2
Vladyslav <dallasx049@gmail.com>
2025-10-24 19:48:24 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+import { join } from 'node:path';
+
+const Statuses = {
+  RESOLVED: 'resolved',
+  ERROR: 'error',
+};
+
+const workerPath = join(import.meta.dirname, 'worker.js');
+const maxCpus = cpus().length;
+
 const performCalculations = async () => {
-  // Write your code here
+  const workers = [];
+  for (let i = 0; i < maxCpus; i++) {
+    workers.push(createPromisifiedWorker(i + 10));
+  }
+
+  const settledWorkers = await Promise.allSettled(workers);
+  const results = settledWorkers.map((p) => p.status === 'fulfilled' ? p.value : p.reason);
+  console.log(results);
+};
+
+/*
+* A function to create promisified version of a worker
+* to preserve the results order
+*  */
+const createPromisifiedWorker = (workerData) => {
+  return new Promise((res, rej) => {
+    const worker = new Worker(workerPath, {
+      workerData,
+    });
+
+    worker.on('error', () => {
+      rej({ status: Statuses.ERROR, data: null });
+    });
+
+    worker.on('message', (message) => {
+      res({ status: Statuses.RESOLVED, data: message });
+    });
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import { workerData, parentPort } from 'node:worker_threads';
+
+// Simulate the worker's rejection
+const rnd = Math.random();
+if (rnd < 0.5) throw new Error();
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
   // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
