70d4b82ed36bb3c08e085262bda731a36484b251
Vladyslav <dallasx049@gmail.com>
2025-10-25 11:36:27 +0300

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { join } from 'node:path';
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const processorPath = join(import.meta.dirname, 'files', 'script.js');
+  const child = spawn(`node`, [processorPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(  [1, 2, 3] );
